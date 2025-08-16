367f1006cd611e924fe342595daac494cb7ea358
Andrei <andreimanacop1@gmail.com>
2025-08-16 21:34:55 +0800

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // enable static export
+  output: "export", // static export
+  images: {
+    unoptimized: true, // disable image optimization for static export
+  },
 };
 
 export default nextConfig;
